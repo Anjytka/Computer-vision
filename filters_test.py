187ac436ca400f1089646d62ac68b0b55a895f3a
NSusoev <nsusoev@mail.ru>
2014-04-03 14:33:14 +0400

    update

--- filters_test.py
+++ filters_test.py
@@ -6,7 +6,7 @@ if __name__ == '__main__':
     img = cv2.imread(sys.argv[1])
     imgFormat = sys.argv[1][len(sys.argv[1]) - 4:]  # parse img format
 
-    for matsize in range(3, 8, 2):
+    for matsize in [3, 5, 9, 15, 21]:
         cv2.imwrite('blur' + str(matsize) +
                     imgFormat, cv2.blur(img, (matsize, matsize)))
         cv2.imwrite('gaus' + str(matsize) +
@@ -15,6 +15,6 @@ if __name__ == '__main__':
                     imgFormat, cv2.medianBlur(img, matsize))
         cv2.imwrite('bilateral' + str(matsize) +
                     imgFormat, cv2.bilateralFilter(img, matsize, 75, 75))
-
-    cv2.imwrite('nonlocal' + imgFormat,
-                cv2.fastNlMeansDenoisingColored(img, None, 10, 10, 7, 21))
+        cv2.imwrite('nonlocal' + str(matsize) + imgFormat,
+                    cv2.fastNlMeansDenoisingColored(img, None, 20, 20,
+                                                    matsize / 3, matsize))
